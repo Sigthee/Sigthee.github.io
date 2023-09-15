94e2f7e3935abc455528f66c5a539f54f434f53b
Sigthee <florian.trips@gmail.com>
2023-09-15 16:02:25 +0200

    commit

---
+++ lettre.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="lettre.css">
+    <title>Mon quoicoubibou au sucré au sucre</title>
+</head>
+<body>
+    <div class="lettre">
+        <div class="gauche">
+            <div class="ligne">
+                <p>Mon quoicoubibou Antho, je t'écrit aujourd'hui de mon quoicouhamac. S'il te plait n'ait pas les cramptés et ne me boude pas bae. Quoicoubisous, ton quoicoubaka au surcre des iles equatoriales situé en 60.3 21.0. Quoicoubisous Julien</p>
+            </div>
+            <p>&copy;|La Carte Parisienne Tél : 03.82.47.10.53</p>
+        </div>
+        <div class="droite">
+            <div class="droitehaut">
+                <img src="./img/tampon.png" alt="" id="tampon">
+                <img src="./img/obliteration.png" alt="" id="obli">
+                <img src="./img/timbre.png" alt="" id="timbre">
+            </div>
+                <div><p>Mon Quoicoubibou Antho</p></div>
+                <div><p>Le Quoicouhamac</p></div>
+                <div><p>QU01C00B3</p></div>
+                <div><p>Quoicourésidence</p></div>
+        </div>
+    </div>
+</body>
+</html>
